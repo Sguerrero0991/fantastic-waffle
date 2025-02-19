4bd3b312d25ca8044539676c9f48d10e739278e2
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-19 13:01:59 -0600

    Made changes to passenger.rb

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,11 +1,35 @@
 require './lib/passenger'
+require 'pry'
 
-# This block helps format the test results
-RSpec.configure do |config|
-  config.formatter = :documentation
+RSpec.describe Passenger do
+  before(:each) do 
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})    
+  end
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@charlie).to be_an_instance_of(Passenger)
+      expect(@taylor).to be_an_instance_of(Passenger)
+    end
+  end
+
+  it 'has attributes' do
+    expect(@charlie.name).to eq("Charlie")
+    expect(@charlie.age).to eq(18)
+  end
+
+  it 'determines if passenger is an adult' do
+    expect(@charlie.adult?).to eq(true)
+    expect(@taylor.adult?).to eq(false)
+  end
+
+  it 'determines if passenger can drive' do
+    expect(@charlie.driver?).to eq(false)
+    @charlie.drive
+    expect(@charlie.driver?).to eq(true)
+  end
+
 end
 
-# Your tests should start here!
-RSpec.describe Passenger do 
-    
-end
+
